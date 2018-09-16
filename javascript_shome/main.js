1914ee26fa87c66225ed4bd681e4219005573d5d
liualbert <liualbert93@gmail.com>
2018-09-17 07:12:56 +0800

    update after final version

---
+++ javascript_shome/main.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function() {
+       var c=0;
+       setInterval(function(){
+           $("#rtate").css("transform","rotate("+c+"deg)");
+           c=c+1;
+       },50);    
+           
+});
+
+ 
+
--- javascript_youtube/playList.js
+++ javascript_youtube/playList.js
@@ -1,14 +1,17 @@
 var playList = [
-    "L229QDxDakU",
-    "SE6RDK2CtIw",
-    "elh6yIC7uVE"
+   // "L229QDxDakU",
+      "9fOw-T4HdRA",
+    //"SE6RDK2CtIw",
+      "RwrT4A_Oh6g",
+    //"elh6yIC7uVE"
+    "L229QDxDakU"
 ];
 
 //播放起迄秒數
 var playTime = [
-    [73,100],
-    [107,123],
-    [116,127]  
+    [5,120],
+    [5,120],
+    [5,120]  
 ];
 
 
